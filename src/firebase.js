ef56d083f66c82aae38d30ad08e6120bd1c81425
Inna Botchkina <innabobolina@yahoo.com>
2021-11-01 14:52:36 -0700

    first commit redux Counter

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0Eu2AdINlGFSEqeDtyEk1ir1hpOxlZo8",
+    authDomain: "netflix-6ee3f.firebaseapp.com",
+    projectId: "netflix-6ee3f",
+    storageBucket: "netflix-6ee3f.appspot.com",
+    messagingSenderId: "185558483256",
+    appId: "1:185558483256:web:67d4f0f249d0fad4ca41cd"
+  };
