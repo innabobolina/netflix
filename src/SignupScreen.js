fcc9e08dabdaf8efb531cd5ef68ec3379d0f7a5f
Inna Botchkina <innabobolina@yahoo.com>
2021-11-14 14:59:46 -0800

    add signIn function

--- src/SignupScreen.js
+++ src/SignupScreen.js
@@ -20,9 +20,20 @@ function SignupScreen() {
         alert(error.message);
       });
   };
+
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => alert(error.message));
   };
+
   return (
     <div className="signupScreen">
       <form>
