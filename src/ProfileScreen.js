833d73281ab30441f31817d736687bce49e74924
Inna Botchkina <innabobolina@yahoo.com>
2021-11-17 15:39:44 -0800

    style profileScreen

--- src/ProfileScreen.js
+++ src/ProfileScreen.js
@@ -13,23 +13,23 @@ function ProfileScreen() {
       <Nav />
       <div className="profileScreen_body">
         <h1>Edit Profile</h1>
-      </div>
-      <div className="profileScreen_info">
-        <img
-          height="50px"
-          className="nav_avatar"
-          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
-          alt="Netflix profile"
-        />
-        <div className="profileScreen_details">
-          <h2>{user.email}</h2>
-          <div className="profileScreen_plans">
-            <button
-              onClick={() => auth.signOut()}
-              className="profileScreen_signOut"
-            >
-              Sign Out
-            </button>
+        <div className="profileScreen_info">
+          <img
+            // height="50px"
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+            alt="Netflix profile"
+          />
+          <div className="profileScreen_details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen_plans">
+              {/* <h3>Plans</h3> */}
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen_signOut"
+              >
+                Sign Out
+              </button>
+            </div>
           </div>
         </div>
       </div>
